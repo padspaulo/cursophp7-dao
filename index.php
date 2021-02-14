6489ad0e905633f75a786743965326441a102f65
padspaulo <pads.paulo.santos@gmail.com>
2021-02-14 03:18:04 +0000

    criamos a classe usuario

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 require_once("config.php");
 
 //Carrega um usuário
-//$root = new Usuario();
-//$root->loadbyId(3);
-//echo $root;
+$root = new Usuario();
+$root->loadbyId(3);
+echo $root;
 
 //Carrega uma lista de usuários
-$lista = Usuario::getList();
-echo json_encode($lista);
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
 //Carrega uma lista de usuários buscando pelo login
 //$search = Usuario::search("jo");
