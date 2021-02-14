185272ea9a8c0124404ff9a74db92d0ed17bf44a
padspaulo <pads.paulo.santos@gmail.com>
2021-02-14 23:02:14 +0000

    metodos de lista

--- index.php
+++ index.php
@@ -3,21 +3,22 @@
 require_once("config.php");
 
 //Carrega um usuário
-$root = new Usuario();
-$root->loadbyId(3);
-echo $root;
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
 //Carrega uma lista de usuários
 //$lista = Usuario::getList();
 //echo json_encode($lista);
 
 //Carrega uma lista de usuários buscando pelo login
-//$search = Usuario::search("jo");
+//$search = Usuario::search("p");
 //echo json_encode($search);
 
 //carrega um usuário usando o login e a senha
-//$usuario = new Usuario();
-//$usuario->login("root", "!@#$");
+$usuario = new Usuario();
+$usuario->login("paulo", "333");
+echo ($usuario);
 
 //echo $usuario;
 /*
